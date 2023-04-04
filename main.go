5ef4ff85b992257ec0f949f243d5ec7ea5e07cbd
Michael Jarvis <michael@jarvis.com>
2023-04-04 09:48:37 -0500

    use zerolog

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	gitUrls "github.com/whilp/git-urls"
 
-	"github.com/mojotx/git-goclone/pkg/msg"
 	"github.com/mojotx/git-goclone/pkg/path"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+		NoColor:    false,
+	})
 	var errCount int
 
 	for _, a := range os.Args[1:] {
@@ -24,25 +32,25 @@ func main() {
 }
 
 func processUrl(gitUrl string) error {
-	fmt.Printf("processing %s...\n", gitUrl)
+	log.Info().Str("url", gitUrl).Msg("processing")
 
 	url, err := gitUrls.Parse(gitUrl)
 	if err != nil {
-		msg.Err("cannot parse %s: %s\n", gitUrl, err.Error())
+		log.Error().Err(err).Str("url", gitUrl).Msg("cannot parse")
 		return err
 	} else if url == nil {
-		msg.Err("this should never happen, but url is a nil pointer (%s)\n", gitUrl)
+		log.Panic().Str("url", gitUrl).Msg("never happen, but url is nil")
 		return fmt.Errorf("url parse failed")
 	}
 
 	// See if the path ends with .git, and if so, remove that before cloning
 	clonePath := path.Sanitize(url.Path)
 
-	msg.Info("Cloning repo %s into %s...\n", gitUrl, clonePath)
+	log.Info().Str("url", gitUrl).Str("clonePath", clonePath).Msg("cloning repo")
 
 	// Clone the repository
 	if _, gErr := git.PlainClone(clonePath, false, &git.CloneOptions{URL: gitUrl, Progress: os.Stderr}); gErr != nil {
-		msg.Err("cannot clone repo %s: %s\n", gitUrl, gErr.Error())
+		log.Error().Err(gErr).Str("url", gitUrl).Str("clonePath", clonePath).Msg("cannot clone repo")
 		return gErr
 	}
 
