755a46a93ed3ba70f86fc3b376478fc1d52451bf
Michael Jarvis <michael@jarvis.com>
2023-04-04 09:57:09 -0500

    use zerolog and simplify logic

--- pkg/path/sanitize.go
+++ pkg/path/sanitize.go
@@ -1,10 +1,7 @@
 package path
 
 import (
-	"os"
 	"regexp"
-
-	"github.com/mojotx/git-goclone/pkg/msg"
 )
 
 // Sanitize is a wrapper around some helper functions that clean up the url path so that we can clone
@@ -16,25 +13,25 @@ func Sanitize(path string) string {
 // PostTrim checks to see if the string ends with the substring ".git", and if so, removes it.
 func PostTrim(path string) string {
 
-	gitPattern := ".git$"
-	if m, err := regexp.MatchString(gitPattern, path); m {
+	// Match \.git$ on the end
+	gitPattern := regexp.MustCompile(`.git$`)
+
+	if gitPattern.MatchString(path) {
 		return path[0 : len(path)-4]
-	} else if err != nil {
-		msg.Err("weird error trying to match '%s' against regexp '%s': %s", path, gitPattern, err.Error())
-		os.Exit(-1)
 	}
+
 	return path
 }
 
 // PreTrim Checks to see if the first character is a path separator, and if so, removes it.
 func PreTrim(path string) string {
+
 	// Check to see if the first character is a path separator, and if so, remove it
-	pathPattern := `^[/\\]`
-	if m, err := regexp.MatchString(pathPattern, path); m {
+	pathPattern := regexp.MustCompile(`^[/\\]`)
+
+	if pathPattern.MatchString(path) {
 		path = path[1:]
-	} else if err != nil {
-		msg.Err("weird error trying to match '%s' against regexp '%s': %s", path, pathPattern, err.Error())
-		os.Exit(-1)
 	}
+
 	return path
 }
